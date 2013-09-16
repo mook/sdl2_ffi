a35b8d2b6b4bdaaf3d0715a431b80da1b8e90f31
BadQuanta <BadQuanta@Gmail.com>
2013-09-16 00:37:38 -0700

    * Better Documentation * Does not require FFI directly, but now #flatten_consts builds an FFI::Enum / `enum :xxx, [:xyz,...]` style array from constants.

--- lib/enumerable_constants.rb
+++ lib/enumerable_constants.rb
@@ -1,6 +1,43 @@
+require 'ffi'
 # This module adds a couple static-scope routines to
 # any module that includes it, allowing them to enumerate
 # the constants they define.  Please help me make it better
+#
+# An example:
+#     module MyEnumeration
+#       include EnumerableConstants
+#       MY_FIRST_CONSTANT = next_const_value # defaults to 0
+#       MY_NEXT_CONSTANT = next_const_value # will be 1
+#       MY_THIRD_CONSTANT = next_const_value # will be 2, and the next would be 3 unless
+#       MY_FOURTH_CONSTANT = 100 # you assign a value.
+#       MY_FIFTH_CONSTANT = next_const_value # will be 101
+#       MY_SIXTH_CONSTANT = MY_THIRD_CONSTANT # There are no rules against duplicate values.
+#       MY_SEVENTH_CONSTANT = next_const_value # will be 3, be careful you don't unintentionally reuse values.
+#     end
+#
+# With this module, you can:
+#
+#     MyEnumeration.by_name #=> {:MY_FIRST_CONSTANT=>0,:MY_NEXT_CONSTANT=>1,:MY_...}
+#
+# This is actually a standard function, nothing I added:
+#
+#     MyEnumeration.constants #=> [:MY_FIRST_CONSTANT,:MY_SECOND_CONSTANT]
+#
+# Get the last value defined:
+#
+#     MyEnumeration.last_const_value #=> 3, based on the example above.
+#
+# Get the next logical value, assuming the last value responds to .next,
+# Otherwise, there will likely be an error.
+# Note: This routine has no effect without assignment to a constant
+# within this EnumerableConstants module/class. i.e.
+#
+#     MyEnumeration::MY_EIGTH_CONSTANT = MyEnumeration.next_const_value # 4
+#
+# However, successive calls to just `#next_const_value` will just result in
+# the same value.
+#     MyEnumeration.next_const_value # 5
+#     MyEnumeration.next_const_value # 5
 module EnumerableConstants
 
   # This is the method that injects the new static-scope routines.
@@ -15,6 +52,12 @@ module EnumerableConstants
           result[constant] = self.const_get(constant)
         end
         return result
+      end
+      
+      # This routine returns a flattened array of alternating symbols and values.
+      # The symbols are the CONSTANT names and values are the values defined by that constant.
+      def self.flatten_consts
+        by_name.to_a.flatten
       end
 
       ##
