4ac5f0799481af70f2f0802ff3c1baee4b60aa75
BadQuanta <BadQuanta@Gmail.com>
2013-09-16 00:48:39 -0700

    * Fixed FFI::Enum defs.

--- lib/sdl2/pixels.rb
+++ lib/sdl2/pixels.rb
@@ -2,7 +2,6 @@ require 'sdl2/error'
 require 'sdl2/color'
 require 'sdl2/pixel_format'
 
-
 # SDL_pixels.h API
 module SDL2
   ALPHA_OPAQUE = 255
@@ -24,7 +23,7 @@ module SDL2
     ARRAYF16 = next_const_value
     ARRAYF32 = next_const_value
   end
-  enum :pixeltype, *PIXELTYPE.by_name
+  enum :pixeltype, PIXELTYPE.flatten_consts
 
   # Bitmap pixel order, high bit -> low bit
   module BITMAPORDER
@@ -33,10 +32,10 @@ module SDL2
     N4321 = next_const_value
     N1234 = next_const_value
   end
-  
-  enum :bitmaporder, *BITMAPORDER::by_name
 
-  # Packed component order, high bit -> low bit 
+  enum :bitmaporder, BITMAPORDER.flatten_consts
+
+  # Packed component order, high bit -> low bit
   module PACKEDORDER
     include EnumerableConstants
     NONE = next_const_value
@@ -49,9 +48,9 @@ module SDL2
     ABGR = next_const_value
     BGRA = next_const_value
   end
-  
-  enum :packedorder, *PACKEDORDER.by_name
-  
+
+  enum :packedorder, PACKEDORDER.flatten_consts
+
   # Array component order, low byte -> hight byte
   module ARRAYORDER
     include EnumerableConstants
@@ -63,11 +62,11 @@ module SDL2
     BGRA = next_const_value
     ABGR = next_const_value
   end
-  
-  enum :arrayorder, *ARRAYORDER.by_name
+
+  enum :arrayorder, ARRAYORDER.flatten_consts
 
   # Packed component layout
-  module PACKEDLAYOUT 
+  module PACKEDLAYOUT
     include EnumerableConstants
     NONE = next_const_value
     N332 = next_const_value
@@ -79,19 +78,20 @@ module SDL2
     N2101010 = next_const_value
     N1010102 = next_const_value
   end
-  
-  enum :packedlayout, *PACKEDLAYOUT.by_name
+
+  enum :packedlayout, PACKEDLAYOUT.flatten_consts
 
   #MACRO: SDL_DEFINE_PIXELFOURCC(A, B, C, D) SDL_FOURCC(A, B, C, D)
   def self.define_pixelfourcc(a,b,c,d)
-    SDL2.fourcc(a,b,c,d); 
+    SDL2.fourcc(a,b,c,d);
   end
 
   #MACRO: SDL_DEFINE_PIXELFORMAT(type, order, layout, bits, bytes) \
   #    ((1 << 28) | ((type) << 24) | ((order) << 20) | ((layout) << 16) | \
   #     ((bits) << 8) | ((bytes) << 0))
   def self.define_pixelformat(type, order, layout, bits, bytes)
-    ((1 << 28) | ((type) << 24) | ((order) << 20) | ((layout) << 16) | ((bits) << 8) | ((bytes) << 0))
+    ((1 << 28) | ((type) << 24) | ((order) << 20) | ((layout) << 16) | 
+     ((bits) << 8) | ((bytes) << 0))
   end
 
   #MACRO: SDL_PIXELFLAG(X)    (((X) >> 28) & 0x0F)
@@ -160,38 +160,38 @@ module SDL2
     INDEX1MSB =     SDL2.define_pixelformat(PIXELTYPE::INDEX1, BITMAPORDER::N1234, 0,    1, 0)
     INDEX4LSB =     SDL2.define_pixelformat(PIXELTYPE::INDEX4, BITMAPORDER::N4321, 0,    4, 0)
     INDEX4MSB =     SDL2.define_pixelformat(PIXELTYPE::INDEX4, BITMAPORDER::N1234, 0,    4, 0)
-    INDEX8 =     SDL2.define_pixelformat(PIXELTYPE::INDEX8, 0, 0, 8, 1)
-    RGB332 =     SDL2.define_pixelformat(PIXELTYPE::PACKED8, PACKEDORDER::XRGB,    PACKEDLAYOUT::N332, 8, 1)
-    RGB444 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N4444, 12, 2)
-    RGB555 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N1555, 15, 2)
-    BGR555 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XBGR,    PACKEDLAYOUT::N1555, 15, 2)
-    ARGB4444 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ARGB,    PACKEDLAYOUT::N4444, 16, 2)
-    RGBA4444 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::RGBA,    PACKEDLAYOUT::N4444, 16, 2)
-    ABGR4444 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ABGR,    PACKEDLAYOUT::N4444, 16, 2)
-    BGRA4444 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::BGRA,    PACKEDLAYOUT::N4444, 16, 2)
-    ARGB1555 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ARGB,    PACKEDLAYOUT::N1555, 16, 2)
-    RGBA5551 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::RGBA,    PACKEDLAYOUT::N5551, 16, 2)
-    ABGR1555 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ABGR,    PACKEDLAYOUT::N1555, 16, 2)
-    BGRA5551 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::BGRA,    PACKEDLAYOUT::N5551, 16, 2)
-    RGB565 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N565, 16, 2)
-    BGR565 =     SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XBGR,    PACKEDLAYOUT::N565, 16, 2)
-    RGB24 =     SDL2.define_pixelformat(PIXELTYPE::ARRAYU8, ARRAYORDER::RGB, 0,    24, 3)
-    BGR24 =     SDL2.define_pixelformat(PIXELTYPE::ARRAYU8, ARRAYORDER::BGR, 0,    24, 3)
-    RGB888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::XRGB,    PACKEDLAYOUT::N8888, 24, 4)
-    RGBX8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::RGBX,    PACKEDLAYOUT::N8888, 24, 4)
-    BGR888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::XBGR,    PACKEDLAYOUT::N8888, 24, 4)
-    BGRX8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::BGRX,    PACKEDLAYOUT::N8888, 24, 4)
-    ARGB8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ARGB,    PACKEDLAYOUT::N8888, 32, 4)
-    RGBA8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::RGBA,    PACKEDLAYOUT::N8888, 32, 4)
-    ABGR8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ABGR,    PACKEDLAYOUT::N8888, 32, 4)
-    BGRA8888 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::BGRA,    PACKEDLAYOUT::N8888, 32, 4)
-    ARGB2101010 =     SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ARGB,    PACKEDLAYOUT::N2101010, 32, 4)
+    INDEX8 =        SDL2.define_pixelformat(PIXELTYPE::INDEX8, 0, 0, 8, 1)
+    RGB332 =        SDL2.define_pixelformat(PIXELTYPE::PACKED8, PACKEDORDER::XRGB,    PACKEDLAYOUT::N332, 8, 1)
+    RGB444 =        SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N4444, 12, 2)
+    RGB555 =        SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N1555, 15, 2)
+    BGR555 =        SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XBGR,    PACKEDLAYOUT::N1555, 15, 2)
+    ARGB4444 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ARGB,    PACKEDLAYOUT::N4444, 16, 2)
+    RGBA4444 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::RGBA,    PACKEDLAYOUT::N4444, 16, 2)
+    ABGR4444 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ABGR,    PACKEDLAYOUT::N4444, 16, 2)
+    BGRA4444 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::BGRA,    PACKEDLAYOUT::N4444, 16, 2)
+    ARGB1555 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ARGB,    PACKEDLAYOUT::N1555, 16, 2)
+    RGBA5551 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::RGBA,    PACKEDLAYOUT::N5551, 16, 2)
+    ABGR1555 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::ABGR,    PACKEDLAYOUT::N1555, 16, 2)
+    BGRA5551 =      SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::BGRA,    PACKEDLAYOUT::N5551, 16, 2)
+    RGB565 =        SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XRGB,    PACKEDLAYOUT::N565, 16, 2)
+    BGR565 =        SDL2.define_pixelformat(PIXELTYPE::PACKED16, PACKEDORDER::XBGR,    PACKEDLAYOUT::N565, 16, 2)
+    RGB24 =         SDL2.define_pixelformat(PIXELTYPE::ARRAYU8,  ARRAYORDER::RGB, 0,    24, 3)
+    BGR24 =         SDL2.define_pixelformat(PIXELTYPE::ARRAYU8,  ARRAYORDER::BGR, 0,    24, 3)
+    RGB888 =        SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::XRGB,    PACKEDLAYOUT::N8888, 24, 4)
+    RGBX8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::RGBX,    PACKEDLAYOUT::N8888, 24, 4)
+    BGR888 =        SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::XBGR,    PACKEDLAYOUT::N8888, 24, 4)
+    BGRX8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::BGRX,    PACKEDLAYOUT::N8888, 24, 4)
+    ARGB8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ARGB,    PACKEDLAYOUT::N8888, 32, 4)
+    RGBA8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::RGBA,    PACKEDLAYOUT::N8888, 32, 4)
+    ABGR8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ABGR,    PACKEDLAYOUT::N8888, 32, 4)
+    BGRA8888 =      SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::BGRA,    PACKEDLAYOUT::N8888, 32, 4)
+    ARGB2101010 =   SDL2.define_pixelformat(PIXELTYPE::PACKED32, PACKEDORDER::ARGB,    PACKEDLAYOUT::N2101010, 32, 4)
 
-    YV12 =     SDL2.define_pixelfourcc('Y', 'V', '1', '2')
-      IYUV =     SDL2.define_pixelfourcc('I', 'Y', 'U', 'V')
-    YUY2 =     SDL2.define_pixelfourcc('Y', 'U', 'Y', '2')
-    UYVY =     SDL2.define_pixelfourcc('U', 'Y', 'V', 'Y')
-    YVYU =     SDL2.define_pixelfourcc('Y', 'V', 'Y', 'U')
+    YV12 =          SDL2.define_pixelfourcc('Y', 'V', '1', '2')
+    IYUV =          SDL2.define_pixelfourcc('I', 'Y', 'U', 'V')
+    YUY2 =          SDL2.define_pixelfourcc('Y', 'U', 'Y', '2')
+    UYVY =          SDL2.define_pixelfourcc('U', 'Y', 'V', 'Y')
+    YVYU =          SDL2.define_pixelfourcc('Y', 'V', 'Y', 'U')
   end
 
   api :SDL_GetPixelFormatName, [:pixel_format], :string
