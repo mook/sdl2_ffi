583b60d9b9c74f6174b4d5b697503bfbc4b4862c
BadQuanta <BadQuanta@Gmail.com>
2013-09-21 17:32:06 -0700

    Implemented Chunk::load_wav,#play, and #playing?

--- lib/sdl2/mixer/chunk.rb
+++ lib/sdl2/mixer/chunk.rb
@@ -19,6 +19,27 @@ module SDL2
        
       member_readers *members
       member_writers *members
+      
+      def self.load_wav(filepath)
+        Mixer.load_wav!(filepath)
+      end
+      
+      # Plays chunk
+      # @param opts[:channel]: defaults to -1
+      # @param opts[:loop]: defaults to 0
+      # @param opts[:ms]: defaults to -1
+      def play(opts = {})
+        channel = opts[:channel] || -1
+        loop = opts[:loop] || 0
+        ms = opts[:ms] || -1
+        Mixer::play_channel_timed(channel, self, loop, ms)
+      end
+      
+      # Check if anything is playing?
+      def self.playing?
+        Mixer::playing?(-1)
+      end
+      
     end
     
   end
